ab10d41df215c1d3059917b25c16b37011183c61
MRKCTRL <davidmbundu@gmail.com>
2024-09-09 14:45:39 +0200

    front end code

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 
 class Config:
-        SECRET_KEY=os.envron.get("SECRET_KEY") or "your_secret_key_here"
+        SECRET_KEY=os.environ.get("SECRET_KEY") or "your_secret_key_here"
         SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URI") or "sqlite://diary.db"
         SQLALCHEMY_TRACK_MODIFICATION=False
